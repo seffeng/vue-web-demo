fc1cb827c417f09cd702d633e92deaedc51cdfd1
seffeng <seffeng@sina.cn>
2020-10-09 09:55:39 +0800

    更新包版本

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,6 @@
  * by https://github.com/PanJiaChen/vue-admin-template
  */
 import { getToken } from '@/utils/auth'
-import { isNull, isExternal } from '@/utils/validate'
 import defaultSettings from '@/settings'
 
 /**
@@ -194,54 +193,4 @@ export function getPageTitle(pageTitle) {
     return `${pageTitle} - ${title}`
   }
   return `${title}`
-}
-
-/**
- * 菜单处理
- * @param {object} router
- */
-export function getMenuItems(router) {
-  const menuItems = []
-  for (const i in router) {
-    if (!isNull(router[i].children) && router[i].children.length > 0) {
-      const children = router[i].children
-      const child = []
-      for (const j in children) {
-        if (isNull(children[j].hidden) || children[j].hidden === false) {
-          child.push({
-            name: children[j].name,
-            path: children[j].path,
-            external: isExternal(children[j].path),
-            meta: children[j].meta
-          })
-        }
-      }
-      if (child.length > 1) {
-        menuItems.push({
-          name: router[i].name,
-          path: router[i].path,
-          external: isExternal(router[i].path),
-          meta: router[i].meta,
-          children: child
-        })
-      } else {
-        for (const j in child) {
-          menuItems.push({
-            name: child[j].name,
-            path: child[j].path,
-            external: isExternal(child[j].path),
-            meta: child[j].meta
-          })
-        }
-      }
-    } else if (isNull(router[i].hidden) || router[i].hidden === false) {
-      menuItems.push({
-        name: router[i].name,
-        path: router[i].path,
-        external: isExternal(router[i].path),
-        meta: router[i].meta
-      })
-    }
-  }
-  return menuItems
 }
