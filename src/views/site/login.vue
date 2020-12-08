18413c691bc440064654ccac92d0c236b56897c0
seffeng <seffeng@sina.cn>
2020-12-08 12:26:49 +0800

    修改登录页面样式

--- src/views/site/login.vue
+++ src/views/site/login.vue
@@ -1,68 +1,66 @@
 <template>
-  <q-layout>
-    <q-page-container>
-      <q-page class="bg-blue-grey-9">
-        <div class="text-h5 text-center">
-          <div class="pt100 q-mb-lg">Login Form</div>
-          <div class="flex flex-center">
-            <q-card>
-              <q-card-section>
-                <q-form
-                  ref="loginForm"
-                  class="q-gutter-md"
-                >
-                  <q-input
-                    v-model="loginForm.username"
-                    lazy-rules
-                    outlined
-                    dense
-                    :rules="loginRules.username"
-                    :no-error-icon="true"
-                    placeholder="请输入账号"
-                  >
-                    <template v-slot:prepend>
-                      <q-icon name="person" />
-                    </template>
-                  </q-input>
+  <div class="q-pa-sm row justify-center bg-blue-grey-8 window-height">
+    <div style="width: 100%; max-width: 400px;">
+      <div class="mt-percent-30 text-h5 q-mb-lg text-center">{{ title }}</div>
+      <q-card class="bg-blue-grey-1">
+        <q-card-section>
+          <q-form
+            ref="loginForm"
+            class="q-gutter-md"
+          >
+            <q-input
+              v-model="loginForm.username"
+              lazy-rules
+              outlined
+              dense
+              :rules="loginRules.username"
+              :no-error-icon="true"
+              placeholder="请输入账号"
+            >
+              <template v-slot:prepend>
+                <q-icon name="person" />
+              </template>
+            </q-input>
 
-                  <q-input
-                    v-model="loginForm.password"
-                    outlined
-                    dense
-                    :type="showPassword()"
-                    lazy-rules
-                    :rules="loginRules.password"
-                    :no-error-icon="true"
-                    placeholder="请输入密码"
-                    @keypress.native.enter="login"
-                  >
-                    <template v-slot:prepend>
-                      <q-icon name="lock" />
-                    </template>
-                    <template v-slot:append>
-                      <q-icon
-                        :name="loginOptions.isShow ? 'visibility' : 'visibility_off'"
-                        class="cursor-pointer"
-                        @click="loginOptions.isShow = !loginOptions.isShow"
-                      />
-                    </template>
-                  </q-input>
-                </q-form>
-                <q-btn color="primary" class="full-width q-mt-md" label="登录" :loading="loading" @click="login" />
-              </q-card-section>
-            </q-card>
-          </div>
-        </div>
-      </q-page>
-    </q-page-container>
-  </q-layout>
+            <q-input
+              v-model="loginForm.password"
+              outlined
+              dense
+              :type="showPassword()"
+              lazy-rules
+              :rules="loginRules.password"
+              :no-error-icon="true"
+              placeholder="请输入密码"
+              @keypress.native.enter="login"
+            >
+              <template v-slot:prepend>
+                <q-icon name="lock" />
+              </template>
+              <template v-slot:append>
+                <q-icon
+                  :name="loginOptions.isShow ? 'visibility' : 'visibility_off'"
+                  class="cursor-pointer"
+                  @click="loginOptions.isShow = !loginOptions.isShow"
+                />
+              </template>
+            </q-input>
+          </q-form>
+          <q-btn color="primary" class="full-width q-mt-md" label="登录" :loading="loading" @click="login" />
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
 </template>
 
 <script>
+import defaultSettings from '@/settings'
+const { title } = defaultSettings
+
 export default {
   name: 'Login',
   data() {
     return {
+      title: title,
       loading: false,
       loginForm: {
         username: '',
@@ -107,7 +105,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.pt100 {
-  padding-top: 150px;
+.mt-percent-30 {
+  margin-top: 30%;
 }
 </style>
